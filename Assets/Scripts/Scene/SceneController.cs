f6c01fc1f170898bc3166cdafda29f6f9e9a6214
plrtp <plrtp@mail.ru>
2026-05-03 13:33:53 +0300

    [UPDATE] 	- Добавил условие перехода из Running в Idle

---
+++ Assets/Scripts/Scene/SceneController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Scer : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
