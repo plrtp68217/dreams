98ba02ec233042bf0663f10385c04cf1953ecf56
plrtp <plrtp@mail.ru>
2026-05-09 21:21:29 +0300

    [UPDATE] 	- Particle System 		- Добавлены капли на земле 		- Настроены слои сортировки 	- Audio 		- NightField 			- Уменьшена громкость 	- DialogSystem 		- JoinTrigger 			- Отключение диалогового окна по таймеру, а не 			  как раньше после выхода из триггерной зоны

--- Assets/Scripts/DialogSystem/JoinTrigger.cs
+++ Assets/Scripts/DialogSystem/JoinTrigger.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class JoinTrigger : MonoBehaviour
@@ -5,20 +6,45 @@ public class JoinTrigger : MonoBehaviour
     [TextArea(3, 10)]
     [SerializeField] private string _text;
     [SerializeField] private Dialog _dialog;
+    [SerializeField] private float _delay = 3f;
+
+
+    private bool _isDialogActive = false;
+    private WaitForSeconds _waiter;
+    private Coroutine _coroutine;
+
+    private void Awake()
+    {
+        _waiter = new WaitForSeconds(_delay);
+    }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.TryGetComponent(out Player _))
+        if (collider.TryGetComponent(out Player _) && _isDialogActive == false)
         {
+            _isDialogActive = true;
+
             _dialog.Enable(_text);
+
+            _coroutine = StartCoroutine(DisableDialog());
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collider)
+    private void OnDisable()
     {
-        if (collider.TryGetComponent(out Player _))
+        if (_coroutine != null)
         {
-            _dialog.Disable();
+            StopCoroutine(_coroutine);
+            _coroutine = null;
         }
+    }
+
+
+    private IEnumerator DisableDialog()
+    {
+        yield return _waiter;
+
+        _dialog.Disable();
+        _isDialogActive = false;
     }
 }
