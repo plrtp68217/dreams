07e6c050996e86762b4afc037378eb4549597ef5
plrtp <plrtp@mail.ru>
2026-05-30 22:04:45 +0300

    [UPDATE] 	- Увеньшил у музыки заднего плана громкость на 83%

--- Assets/Scripts/Audio/AudioController.cs
+++ Assets/Scripts/Audio/AudioController.cs
@@ -2,39 +2,41 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
-    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioSource _audioSourceMain;
     [SerializeField] private AudioClip _ambientClip;
+
+    [SerializeField] private AudioSource _audioSourceBG;
     [SerializeField] private AudioClip _backgroundClip;
 
     [SerializeField][Range(0f, 1f)] private float _volume = 0.5f;
 
     public float InitialVolume => _volume;
-    public float SourceVolume => _audioSource.volume;
+    public float SourceVolume => _audioSourceMain.volume;
 
     public void StartAudio()
     {
-        if (_audioSource == null || _ambientClip == null)
+        if (_audioSourceMain != null && _ambientClip != null)
         {
-            return;
+            _audioSourceMain.loop = true;
+            _audioSourceMain.resource = _ambientClip;
+            _audioSourceMain.Play();
         }
 
-        _audioSource.loop = true;
-        _audioSource.resource = _ambientClip;
-        _audioSource.Play();
-
-        if (_backgroundClip != null)
+        if (_audioSourceBG != null && _backgroundClip != null)
         {
-            _audioSource.PlayOneShot(_backgroundClip, _volume);
+            _audioSourceBG.loop = true;
+            _audioSourceBG.resource = _backgroundClip;
+            _audioSourceBG.Play();
         }
     }
 
     public void SetVolume(float volume)
     {
-        if (_audioSource == null)
+        if (_audioSourceMain == null)
         {
             return;
         }
 
-        _audioSource.volume = Mathf.Clamp01(volume);
+        _audioSourceMain.volume = Mathf.Clamp01(volume);
     }
 }
